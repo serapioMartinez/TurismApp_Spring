76fe60f6df334dc3b73fd49f4c3d1ac5714773ec
Luis Serapio <luisserapiomartinez@gmail.com>
2024-11-14 22:17:02 -0600

    JWT token generation

--- src/main/java/com/radical3d/turismapp/TurismApp/security/SecurityUtils.java
+++ src/main/java/com/radical3d/turismapp/TurismApp/security/SecurityUtils.java
@@ -1,9 +1,25 @@
 package com.radical3d.turismapp.TurismApp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import com.radical3d.turismapp.TurismApp.repository.ICityRepository;
 
 @Component
 public class SecurityUtils {
+
+    @Autowired
+    ICityRepository cityRepository;
+    
+    public int getcityIDForAuthenticatedUser() {
+        return cityRepository
+                    .findCityIDByUsername(
+                        getUserDetailsFromSecurityContext().getUsername())
+                    .get();
+    }
+
     private UserDetails getUserDetailsFromSecurityContext(){
         return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
--- src/main/java/com/radical3d/turismapp/TurismApp/service/ICityService.java
+++ src/main/java/com/radical3d/turismapp/TurismApp/service/ICityService.java
@@ -1,5 +1,10 @@
 package com.radical3d.turismapp.TurismApp.service;
 
-public interface ICityService {
+import com.radical3d.turismapp.TurismApp.model.City;
 
+public interface ICityService {
+    public City createCity(City city);
+    public City updateCity(City city);
+    public City getCity();
+    public Integer deleteCity(int id);
 }
